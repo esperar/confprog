84eba5885b8546f1144838aa913ffc7b9ea26eca
aesperer <piyrw9754@gmail.com>
2023-02-25 01:54:05 +0900

    :art: JSON toDouble
    todoubel

--- src/main/kotlin/esperer/protoconfig/json/JSON.kt
+++ src/main/kotlin/esperer/protoconfig/json/JSON.kt
@@ -1,9 +1,44 @@
 package esperer.protoconfig.json
 
+import java.lang.NumberFormatException
+
 class JSON {
-    open fun checkDouble(d: Double): Double {
+
+    fun checkDouble(d: Double): Double {
         if(Double.POSITIVE_INFINITY == d || Double.NaN == d)
             throw JSONException("Forbidden numeric value: $d")
         return d;
+    }
+
+    fun toBoolean(value: Any?): Boolean?{
+        if(value is Boolean)
+            return value as Boolean
+
+        if(value is String){
+            var stringValue: String = value as String
+            if("true".equals(stringValue))
+                return true
+            if("false".equals(stringValue))
+                return false
+        }
+
+        return null
+    }
+
+    fun toDouble(value: Any?): Double? {
+        if(value is Double){
+            return value as Double
+        }
+        if(value is Number){
+            return (value as Number).toDouble()
+        }
+        if(value is String){
+            try{
+                return value.toDouble()
+            } catch (ignored: NumberFormatException){
+
+            }
+        }
+        return null
     }
 }
