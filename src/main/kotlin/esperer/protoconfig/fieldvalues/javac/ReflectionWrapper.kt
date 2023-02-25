f09284e4f5b669327b71f68ab5f5997020d8562e
aesperer <piyrw9754@gmail.com>
2023-02-25 13:23:40 +0900

    :sparkles: ReflectionWrapper Tree
    Tree

--- src/main/kotlin/esperer/protoconfig/fieldvalues/javac/ReflectionWrapper.kt
+++ src/main/kotlin/esperer/protoconfig/fieldvalues/javac/ReflectionWrapper.kt
@@ -1,4 +1,41 @@
 package esperer.protoconfig.fieldvalues.javac
 
-class ReflectionWrapper {
+import java.lang.Exception
+import java.lang.IllegalStateException
+import java.lang.reflect.Method
+
+
+open class ReflectionWrapper(
+    private val type: Any?,
+    private val instance: Any?
+) {
+    constructor() : this(null, null)
+
+    fun findClass(name: String) {
+        this.instance?.javaClass?.let { findClass(it.classLoader, name) }
+    }
+
+    fun findMethod(name: String, parameterType: Class<*>): Method{
+        return findMethod(this.type as Class<*>, name, parameterType)
+    }
+
+    fun findClass(classLoader: ClassLoader, name: String): Class<*> {
+        try{
+            return Class.forName(name, false, classLoader)
+        } catch(e: ClassNotFoundException){
+            throw IllegalAccessException()
+        }
+    }
+
+    override fun toString(): String {
+        return this.instance.toString()
+    }
+
+    fun findMethod(type: Class<*>, name: String, parameterType: Class<*>): Method{
+        try{
+            return type.getMethod(name, parameterType)
+        } catch (e: Exception){
+            throw IllegalStateException()
+        }
+    }
 }
--- src/main/kotlin/esperer/protoconfig/fieldvalues/javac/Tree.kt
+++ src/main/kotlin/esperer/protoconfig/fieldvalues/javac/Tree.kt
@@ -1,4 +1,4 @@
 package esperer.protoconfig.fieldvalues.javac
 
-class Tree {
+class Tree: ReflectionWrapper() {
 }
